792117c3849012c38b3a9489e43220f389add370
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2025-01-25 22:33:57 +0000

    updated color

--- app/src/main/java/com/android/clearscore/ui/theme/Color.kt
+++ app/src/main/java/com/android/clearscore/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.android.clearscore.ui.theme
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
+// Default from project
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
