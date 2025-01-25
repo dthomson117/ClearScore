ebb7ffebe2800a2c2f5861df65b2adbac0df666a
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2025-01-25 22:43:22 +0000

    removed example tests, re-added default companion object

--- domain/src/main/java/com/android/domain/model/CreditScore.kt
+++ domain/src/main/java/com/android/domain/model/CreditScore.kt
@@ -2,4 +2,10 @@ package com.android.domain.model
 
 data class CreditScore(
     val creditReportInfo: CreditReportInfo
-)
+) {
+    companion object {
+        fun default() = CreditScore(
+            creditReportInfo = CreditReportInfo.default()
+        )
+    }
+}
